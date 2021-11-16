4ff6731e85253c322ad5267c3621c0376dcca6c7
aidana21 <93640059+aidana21@users.noreply.github.com>
2021-11-16 20:38:47 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Aidana</title>
+  <link rel="stylesheet"  href="main.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="WholePort">
+  <img src="https://i.pinimg.com/564x/a0/1a/e8/a01ae835831e6951a202b9b1c5d4ac91.jpg" alt="Me">
+  <p>Айдана, 17 лет учусь в колледже Ала-Тоо на танцы и курсы английского.<br>В свободное время люблю гулять и смотреть сериалы<br>
+  Aдрес- Московская 85 Номер- 0707-78-18-08<br></p>
+</div>
+<div class="Icons">
+  <a href="https://www.youtube.com/" class="fa fa-facebook"></a>
+  <a href="#" class="fa fa-instagram"></a>
+  <a href="#" class="fa fa-youtube"></a>
+</div>
+</body>
+</html>
